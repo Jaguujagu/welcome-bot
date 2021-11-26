ce9ba6aa6c120ce874c0efe19d06578681e06b14
Jaguujagu <95091731+Jaguujagu@users.noreply.github.com>
2021-11-26 22:02:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,10 @@ Token =os.environ.get("BOT_TOKEN",None)
 updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('''Hi iam welcome messanger bot 
-Add me to your group 
- 
- Made with Love ❤️ by @Tamil_Telugu_Kanada_Hindi_Movies
+ updater.message.reply_text('''Hi iam 👑QUEEN👑 from SCUK NETWORK
+Add me to your group for welcome your group members
+Made with Love ❤️ 
+by @Tamil_Telugu_Kanada_Hindi_Movies
 
   ''')
 def help(updater,context):
